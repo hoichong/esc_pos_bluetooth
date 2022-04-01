b8a8160cc045936383c1077694340f0b2fb5dd35
Hoi Chong <hoichong@w3ocean.com>
2022-04-01 12:23:54 +0700

    Update to use flutter_ bluetooth_plus addon

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -10,7 +10,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:esc_pos_utils/esc_pos_utils.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
+import 'package:flutter_bluetooth_plus/flutter_bluetooth_plus.dart';
 import './enums.dart';
 
 /// Bluetooth printer
